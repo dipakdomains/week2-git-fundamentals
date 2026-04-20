9959dd2934bb66c06d1e28c8983ea4af041e812a
dipakdomains <dipak.domains@gmail.com>
2026-04-20 15:36:23 -0400

    Enhance About section in feature branch

--- week2-git-lab/week8-build-lab/src/script.js
+++ week2-git-lab/week8-build-lab/src/script.js
@@ -5,3 +5,10 @@ function calculateTotal(a, b) {
  return total;
 }
 console.log(calculateTotal(5, 10));
+
+// New function added in feature branch
+function showWelcomeMessage() {
+  console.log("Welcome to my enhanced About section!");
+}
+
+showWelcomeMessage();
